d032b2d3f9f4e65ae67f1e839d227cac307f8ba4
Hulkiniano <hulkiniano@gmail.com>
2025-04-24 10:26:51 +0200

    TresEnRaya.java

--- TresEnRaya.java
+++ TresEnRaya.java
@@ -1,12 +1,4 @@
-package Examen;
-
-
 import java.util.Scanner;
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 /**
  *
  * @author hulkiniano
